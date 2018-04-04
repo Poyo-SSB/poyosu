c8510e668ac7ca8d4428043120462c5eb8e37e2e
Poyo <poyoarya@gmail.com>
2018-04-03 18:39:07 -0700

    Added pause/results buttons

---
+++ buttons.jsx
@@ -0,0 +1,36 @@
+﻿var document = app.activeDocument;
+
+function colorFromHex(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    var color = new RGBColor();
+    color.red = parseInt(result[1], 16);
+    color.green = parseInt(result[2], 16);
+    color.blue = parseInt(result[3], 16);
+    return color;
+}
+
+function exportFile(name, width, height) {
+    var exportOptions = new ExportOptionsPNG24();
+    exportOptions.matte = false;
+    exportOptions.horizontalScale = (width / document.width) * 100;
+    exportOptions.verticalScale = (height / document.height) * 100;
+    
+    var path = document.path + "/" + name + ".png"
+    
+    document.exportFile(new File(path), ExportType.PNG24, exportOptions);
+}
+
+var background = document.pathItems.getByName("Background");
+var textItem = document.textFrames.getByName("Text");
+
+function process(hex, text, file) {
+    background.fillColor = colorFromHex(hex);
+    textItem.contents = text;
+    exportFile(file + "@2x", 1000, 180);
+    exportFile(file, 500, 90);
+}
+
+process("#e8193b", "Back to Menu", "pause-back");
+process("#f69100", "Retry", "pause-retry");
+process("#a2c80c", "Continue", "pause-continue");
+process("#0078f2", "Watch Replay", "pause-replay");
