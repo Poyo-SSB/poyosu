3bb1906d3a6d8f9ac8d914eddb5b0b397f7e66fa
Poyo <poyoarya@gmail.com>
2018-04-03 18:51:27 -0700

    Added countdown text

---
+++ countdown.jsx
@@ -0,0 +1,26 @@
+﻿var document = app.activeDocument;
+
+function exportFile(name, width, height) {
+    var exportOptions = new ExportOptionsPNG24();
+    exportOptions.matte = false;
+    exportOptions.artBoardClipping = true;
+    exportOptions.horizontalScale = (width / document.width) * 100;
+    exportOptions.verticalScale = (height / document.height) * 100;
+    
+    var path = document.path + "/" + name + ".png"
+    
+    document.exportFile(new File(path), ExportType.PNG24, exportOptions);
+}
+
+var textItem = document.textFrames.getByName("Text");
+
+function process(text, file) {
+    textItem.contents = text;
+    exportFile(file + "@2x", 800, 500);
+    exportFile(file, 400, 250);
+}
+
+process("3", "count3");
+process("2", "count2");
+process("1", "count1");
+process("Go!", "go");
