359bfbc2dd935b05ce7078bf1e4e2b217cf046db
Poyo <poyoarya@gmail.com>
2018-04-08 18:33:01 -0700

    Added reverse arrow

---
+++ reversearrow.jsx
@@ -0,0 +1,18 @@
+﻿var document = app.activeDocument;
+
+function exportFile(name, width, height) {
+    var exportOptions = new ExportOptionsPNG24();
+    exportOptions.matte = false;
+    exportOptions.artBoardClipping = true;
+    exportOptions.horizontalScale = (width / document.width) * 100;
+    exportOptions.verticalScale = (height / document.height) * 100;
+    
+    var path = document.path + "/" + name + ".png"
+    
+    document.exportFile(new File(path), ExportType.PNG24, exportOptions);
+}
+
+// I don't know how to export normally and I don't care enough to find out how. Scripts forever. B)
+
+exportFile("reversearrow@2x", 120, 120);
+exportFile("reversearrow", 60, 60);
