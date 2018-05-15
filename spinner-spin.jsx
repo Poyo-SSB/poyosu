bf21274845c9bce9bf4df730c1ef7d89882b9d70
Poyo <poyoarya@gmail.com>
2018-05-14 17:06:45 -0700

    Added spinner stuff

---
+++ spinner-spin.jsx
@@ -0,0 +1,16 @@
+﻿var document = app.activeDocument;
+
+function exportFile(name, width, height) {
+    var exportOptions = new ExportOptionsPNG24();
+    exportOptions.matte = false;
+    exportOptions.artBoardClipping = true;
+    exportOptions.horizontalScale = (width / document.width) * 100;
+    exportOptions.verticalScale = (height / document.height) * 100;
+    
+    var path = document.path + "/" + name + ".png"
+    
+    document.exportFile(new File(path), ExportType.PNG24, exportOptions);
+}
+
+exportFile("spinner-spin@2x", 650, 250);
+exportFile("spinner-spin", 325, 125);
