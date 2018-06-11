aa9d6470a30b8c47ee03e8a4b0e1aee39bcefbc0
Poyo <poyoarya@gmail.com>
2018-06-10 20:33:15 -0700

    Reverted silly spinners

--- shared/spinner.jsx
+++ shared/spinner.jsx
@@ -14,18 +14,12 @@ function exportFile(name, width, height) {
 
 var bottom = document.layers.getByName("Bottom");
 var middle2 = document.layers.getByName("Middle2");
-var top = document.layers.getByName("Top");
 
 bottom.visible = true;
 middle2.visible = false;
-top.visible = false;
 exportFile("spinner-bottom@2x", 1190, 1190);
 exportFile("spinner-bottom", 1190, 1190);
 bottom.visible = false;
 middle2.visible = true;
 exportFile("spinner-middle2@2x", 1190, 1190);
 exportFile("spinner-middle2", 1190, 1190);
-middle2.visible = false;
-top.visible = true;
-exportFile("spinner-top@2x", 1190, 1190);
-exportFile("spinner-top", 1190, 1190);
