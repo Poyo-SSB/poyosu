42e3346449153a642c7841f84c350ffb963e483e
Poyo <poyoarya@gmail.com>
2018-06-24 19:27:47 -0700

    Migrated numbers

--- scripts/default-numbers.jsx
+++ scripts/default-numbers.jsx
@@ -17,8 +17,8 @@ var textItem = document.textFrames.getByName("Text");
 function process(text) {
     textItem.contents = text;
     document.artboards[0].artboardRect = [0, 0, textItem.width, -100]
-    exportFile("default-" + text + "@2x", textItem.width, 100, 0.95);
-    exportFile("default-" + text, textItem.width / 2, 50, 0.475);
+    exportFile("../shared/default-" + text + "@2x", textItem.width, 100, 0.95);
+    exportFile("../shared/default-" + text, textItem.width / 2, 50, 0.475);
 }
 
 for (var i = 0; i < 10; i++) {
