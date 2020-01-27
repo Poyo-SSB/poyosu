4caf0b476fa04b25ffd6b3097e47757c9dc4ea17
Poyo <poyoarya@gmail.com>
2020-01-26 16:04:39 -0800

    Streamline cursor trail builder

--- poyosu/Builders/CursorTrailBuilder.cs
+++ poyosu/Builders/CursorTrailBuilder.cs
@@ -25,16 +25,7 @@ namespace poyosu.Builders
                 return;
             }
 
-            int minSize = 10;
-            Rgba32 color = parameters.CursorTrailColor;
-            float radius = parameters.CursorTrailRadius;
-
-            if (parameters.CursorTrailUltrasmooth)
-            {
-                radius /= 2;
-            }
-
-            int size = (int)(radius * 2);
+            int size = (int)(parameters.CursorTrailRadius * 2);
 
             var colors = new ColorStop[gaussian_samples];
 
@@ -43,30 +34,34 @@ namespace poyosu.Builders
                 float sample = ImageMath.GaussianFunction((float)i / gaussian_samples, 1, 0, 1 / 3f);
 
                 colors[i] = new ColorStop((float)i / gaussian_samples, new Rgba32(
-                    color.R,
-                    color.G,
-                    color.B,
-                    (byte)Math.Round(sample * color.A)));
+                    parameters.CursorTrailColor.R,
+                    parameters.CursorTrailColor.G,
+                    parameters.CursorTrailColor.B,
+                    (byte)Math.Round(sample * parameters.CursorTrailColor.A)));
             }
 
             using var trail = new Image<Rgba32>(size, size);
 
-            var center = new PointF(radius, radius);
+            var center = new PointF(parameters.CursorTrailRadius, parameters.CursorTrailRadius);
 
             var brush = new EllipticGradientBrush(
                 new PointF(size / 2f, size / 2f),
                 new PointF(size, size / 2f),
                 1, GradientRepetitionMode.None, colors);
 
-            trail.Mutate(ctx => ctx.Fill(brush, new EllipsePolygon(center, radius)));
+            trail.Mutate(ctx => ctx.Fill(brush, new EllipsePolygon(center, parameters.CursorTrailRadius)));
 
-            if (size < minSize)
+            if (size < 10)
             {
-                Logger.Log($"The cursor trail size is less than {minSize}--the trail may not render in-game. Adjust cursor_trail_radius to fix this.", Logger.MessageType.Warning);
+                Logger.Log($"The cursor trail size is less than 10--the trail may not render in-game. Adjust cursor_trail_radius to fix this.", Logger.MessageType.Warning);
             }
 
             trail.SaveToFileWithHD(System.IO.Path.Combine(path, $"cursortrail"), parameters.HD);
-            Assets.ImageBlank.SaveToFileWithHD(System.IO.Path.Combine(path, $"cursormiddle"), parameters.HD);
+
+            if (parameters.CursorTrailSmooth)
+            {
+                Assets.ImageBlank.SaveToFileWithHD(System.IO.Path.Combine(path, $"cursormiddle"), parameters.HD);
+            }
 
             await Task.CompletedTask;
         }
