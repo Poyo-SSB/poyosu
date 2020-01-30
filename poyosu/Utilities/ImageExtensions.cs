45970e6daceff16a0679b2a67513dae9e4c78dbe
Poyo <poyoarya@gmail.com>
2020-01-30 09:10:27 -0800

    Allow selective trimming

--- poyosu/Utilities/ImageExtensions.cs
+++ poyosu/Utilities/ImageExtensions.cs
@@ -9,94 +9,108 @@ namespace poyosu.Utilities
 {
     public static class ImageExtensions
     {
-        public static void Trim(this Image<Rgba32> source)
+        public static void Trim(this Image<Rgba32> source) => source.Trim(TrimSide.All);
+
+        public static void Trim(this Image<Rgba32> source, TrimSide side)
         {
-            int left;
-            int top;
-            int width;
-            int height;
+            int left = 0;
+            int top = 0;
+            int width = source.Width;
+            int height = source.Height;
 
-            for (left = 0; left < source.Width; left++)
+            if (side.HasFlag(TrimSide.Left))
             {
-                bool transparent = true;
-                for (int y = 0; y < source.Height; y++)
+                for (left = 0; left < source.Width; left++)
                 {
-                    Rgba32 pixel = Rgba32.Transparent;
-                    source[left, y].ToRgba32(ref pixel);
-
-                    if (pixel.A != 0)
+                    bool transparent = true;
+                    for (int y = 0; y < source.Height; y++)
                     {
-                        transparent = false;
+                        Rgba32 pixel = Rgba32.Transparent;
+                        source[left, y].ToRgba32(ref pixel);
+
+                        if (pixel.A != 0)
+                        {
+                            transparent = false;
+                            break;
+                        }
+                    }
+
+                    if (!transparent)
+                    {
                         break;
                     }
-                }
-
-                if (!transparent)
-                {
-                    break;
                 }
             }
 
-            for (top = 0; top < source.Height; top++)
+            if (side.HasFlag(TrimSide.Top))
             {
-                bool transparent = true;
-                for (int x = 0; x < source.Width; x++)
+                for (top = 0; top < source.Height; top++)
                 {
-                    Rgba32 pixel = Rgba32.Transparent;
-                    source[x, top].ToRgba32(ref pixel);
-
-                    if (pixel.A != 0)
+                    bool transparent = true;
+                    for (int x = 0; x < source.Width; x++)
                     {
-                        transparent = false;
+                        Rgba32 pixel = Rgba32.Transparent;
+                        source[x, top].ToRgba32(ref pixel);
+
+                        if (pixel.A != 0)
+                        {
+                            transparent = false;
+                            break;
+                        }
+                    }
+
+                    if (!transparent)
+                    {
                         break;
                     }
-                }
-
-                if (!transparent)
-                {
-                    break;
                 }
             }
 
-            for (width = 0; width < source.Width; width++)
+            if (side.HasFlag(TrimSide.Right))
             {
-                bool transparent = true;
-                for (int y = 0; y < source.Height; y++)
+                for (width = 0; width < source.Width; width++)
                 {
-                    Rgba32 pixel = Rgba32.Transparent;
-                    source[left + width, y].ToRgba32(ref pixel);
-
-                    if (pixel.A != 0)
+                    bool transparent = true;
+                    for (int y = 0; y < source.Height; y++)
                     {
-                        transparent = false;
+                        Rgba32 pixel = Rgba32.Transparent;
+                        source[left + width, y].ToRgba32(ref pixel);
+
+                        if (pixel.A != 0)
+                        {
+                            transparent = false;
+                            break;
+                        }
+                    }
+
+                    if (transparent)
+                    {
                         break;
                     }
-                }
-
-                if (transparent)
-                {
-                    break;
                 }
             }
 
-            for (height = 0; height < source.Height; height++)
+            if (side.HasFlag(TrimSide.Bottom))
             {
-                bool transparent = true;
-                for (int x = 0; x < source.Width; x++)
+                for (height = 0; height < source.Height; height++)
                 {
-                    Rgba32 pixel = Rgba32.Transparent;
-                    source[x, top + height].ToRgba32(ref pixel);
-
-                    if (pixel.A != 0)
+                    bool transparent = true;
+                    for (int x = 0; x < source.Width; x++)
                     {
-                        transparent = false;
+                        Rgba32 pixel = Rgba32.Transparent;
+                        source[x, top + height].ToRgba32(ref pixel);
+
+                        if (pixel.A != 0)
+                        {
+                            transparent = false;
+                            break;
+                        }
+                    }
+
+                    if (transparent)
+                    {
                         break;
                     }
-                }
-
-                if (transparent)
-                {
-                    break;
                 }
             }
 
---
+++ poyosu/Utilities/TrimSide.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poyosu.Utilities
+{
+    [Flags]
+    public enum TrimSide
+    {
+        Left = 1 << 0,
+        Right = 1 << 1,
+        Top = 1 << 2,
+        Bottom = 1 << 3,
+        Horizontal = Left | Right,
+        Vertical = Top | Bottom,
+        All = Horizontal | Vertical,
+    }
+}
